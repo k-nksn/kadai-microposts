9039a1ad8d9ba560a28eed1185847fe019df11a6
k-nksn <k.nksn0540@gmail.com>
2018-11-06 08:04:09 +0000

    modified microposts.blade

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -17,6 +17,7 @@
                     {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
                     {!! Form::close() !!}
+                @endif
             </div>
         </div>
     </li>
