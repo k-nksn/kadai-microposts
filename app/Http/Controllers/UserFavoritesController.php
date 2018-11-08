1b2012dc6aeb51d3aec46b7554a9581857280ff6
k-nksn <k.nksn0540@gmail.com>
2018-11-08 06:46:40 +0000

    UserFavorites

--- app/Http/Controllers/UserFavoritesController.php
+++ app/Http/Controllers/UserFavoritesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserFavoritesController extends Controller
 {
-    public function store(Request $request, $id)
+    public function store($id)
     {
         \Auth::user()->favorite($id);
         return redirect()->back();
