e9fdae5c390b0c69c8883ae21b932e905bebdeaa
k-nksn <k.nksn0540@gmail.com>
2018-11-08 04:41:22 +0000

    favorites_button

--- resources/views/favorites/favorites_button.blade.php
+++ resources/views/favorites/favorites_button.blade.php
@@ -1,10 +1,10 @@
 @if (Auth::id() != $microposts->id)
     @if (Auth::user()->is_favorites($microposts->id))
-        {!! Form::open(['route' => ['user.unfavorite', $microposts->id], 'method' => 'delete']) !!}
+        {!! Form::open(['route' => ['users.unfavorites', $microposts->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorites', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
-        {!! Form::open(['route' => ['user.favorite', $microposts->id]]) !!}
+        {!! Form::open(['route' => ['users.favorite', $microposts->id]]) !!}
             {!! Form::submit('Favorites', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
