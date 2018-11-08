52c4fa5e5aecf23e664995e89b008eea089e55d2
k-nksn <k.nksn0540@gmail.com>
2018-11-08 05:46:46 +0000

    modified button

--- resources/views/favorites/favorites_button.blade.php
+++ resources/views/favorites/favorites_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_favorite($user->id))
         {!! Form::open(['route' => ['user.unfavorite', $user->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favorite', $user->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-outline-secondary btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-outline-secondary"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
