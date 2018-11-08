d1dc341a838cd690311b27ba9773f696a6a12b47
k-nksn <k.nksn0540@gmail.com>
2018-11-08 05:12:26 +0000

    check

--- resources/views/favorites/favorites_button.blade.php
+++ resources/views/favorites/favorites_button.blade.php
@@ -1,11 +1,11 @@
-@if (Auth::id() != $microposts->id)
-    @if (Auth::user()->is_favorites($microposts->id))
-        {!! Form::open(['route' => ['users.unfavorites', $microposts->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorites', ['class' => "btn btn-danger btn-block"]) !!}
+@if (Auth::id() != $user->id)
+    @if (Auth::user()->is_following($user->id))
+        {!! Form::open(['route' => ['user.unfavorite', $user->id], 'method' => 'delete']) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
-        {!! Form::open(['route' => ['users.favorite', $microposts->id]]) !!}
-            {!! Form::submit('Favorites', ['class' => "btn btn-primary btn-block"]) !!}
+        {!! Form::open(['route' => ['user.favorite', $user->id]]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,7 +12,7 @@
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
-            <div>
+            <div>microposts.blade
                 @if (Auth::id() == $micropost->user_id)
                     {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
