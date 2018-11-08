1be9e1228ec4cdeffe20075b9f05b98ebf37f966
k-nksn <k.nksn0540@gmail.com>
2018-11-08 05:15:47 +0000

    microposts.blade

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,7 +12,8 @@
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
-            <div>microposts.blade
+            <div>
+                @include('favorites.favorites_button', ['users' => $micropost])
                 @if (Auth::id() == $micropost->user_id)
                     {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
